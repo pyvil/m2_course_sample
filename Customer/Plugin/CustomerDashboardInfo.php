c47f0d1010774d96f4752f5a41eed7c9597b5ba0
vipya <vipya@smile.fr>
2020-10-15 14:48:32 +0300

    added multiselect

--- Customer/Plugin/CustomerDashboardInfo.php
+++ Customer/Plugin/CustomerDashboardInfo.php
@@ -57,6 +57,6 @@ class CustomerDashboardInfo
             $data = $this->scopeConfig->getValue('god_mode/details/after_customer_name');
         }
 
-        return __('%1 => (%2)', $result, $data);
+        return __("%1 => (%2) \n %3", $result, $data, $this->scopeConfig->getValue('god_mode/details/allowed_sizes'));
     }
 }
