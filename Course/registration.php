43ee3d7f41d7e76721c77617a7cb6ffafbff1d35
vipya <vipya@smile.fr>
2020-11-13 16:34:29 +0200

    ui grid

--- Course/registration.php
+++ Course/registration.php
@@ -15,3 +15,14 @@ ComponentRegistrar::register(
     'Smile_Course',
     __DIR__
 );
+
+/*
+ *
+php bin/magento setup:install --db-host="m2_contribute_mysql" --db-name="magento" --db-user="magento" --db-password="magento" --base-url="http://m2-contribute.loc:30080" --admin-user="admin" --admin-password="magent0" --admin-email="admin@m2-contribute.loc" --admin-firstname="Admin" --admin-lastname="Admin" --use-rewrites=1 --cleanup-database
+
+php bin/magento bin/magento setup:upgrade
+
+php bin/magento sampledata:deploy
+
+bin/magento setup:db-declaration:generate-whitelist
+ */
